92736512e6d9f8944c5d9d49913065e674b33756
Dovekie <dovekie@gmail.com>
2015-10-24 11:06:37 -0700

    debugging database initialization

--- seed.py
+++ seed.py
@@ -4,6 +4,7 @@ from model import Bird, connect_to_db, db
 import json
 from server import app
 import requests
+import pprint
 
 def load_birds():
     """
@@ -25,6 +26,7 @@ def load_birds():
         binom = bird["sciName"]
         ebirds[binom] = (bird_id, common_name)
 
+
     # These two lines get the full faircloth json. commented out for testing purposes.
     faircloth_response = requests.get("http://birds.faircloth-lab.org/api/v1/species/?offset=0&limit=20000")
     faircloth_data = faircloth_response.json()
@@ -37,12 +39,26 @@ def load_birds():
     # match the faircloth data to the ebird data, and use both to create a new row in the birds table.
     # might be a good idea to convert this to ascii (it's unicode right now)
     # at the moment, ascii conversion is handled by server.py in /search GET route
+
+    birds_dict = {}
+
+    
     for bird in faircloth_birds:
         if bird["binomial"] in ebirds:
-            binomial = bird["binomial"]
+            # birds_dict[bird["binomial"]] = {}
+            # birds_dict[bird["binomial"]]["taxon_id"] = ebirds[bird["binomial"]][0]
+            # birds_dict[bird["binomial"]]["common_name"] = ebirds[bird["binomial"]][1]
+            # birds_dict[bird["binomial"]]["sci_name"] = bird["binomial"]
+            # birds_dict[bird["binomial"]]["sp_species"] = bird["species"]
+            # birds_dict[bird["binomial"]]["sp_genus"] = bird["genus"]
+            # birds_dict[bird["binomial"]]["sp_family"] = bird["family"],
+            # birds_dict[bird["binomial"]]["sp_order"] = bird["order"].title()
+            # birds_dict[bird["binomial"]]["region"] = bird["breed_region"]
+            # birds_dict[bird["binomial"]]["subregion"] = bird["breed_subregion"]
+            # birds_dict[bird["binomial"]]["nonbreeding_region"] = bird["nonbreed"]
 
-            new_bird = Bird(taxon_id = ebirds[binomial][0],
-                            common_name = ebirds[binomial][1],
+            new_bird = Bird(taxon_id = ebirds[bird["binomial"]][0],
+                            common_name = ebirds[bird["binomial"]][1],
                             sci_name = bird["binomial"],
                             sp_species = bird["species"],
                             sp_genus = bird["genus"],
@@ -54,7 +70,8 @@ def load_birds():
                             )
             db.session.add(new_bird)
     db.session.commit()
-
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(birds_dict)
 if __name__ == "__main__":
     connect_to_db(app)  
 
